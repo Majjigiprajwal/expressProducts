6d10e9344d0e10eb07f4f189c0bef8b8ea3121f1
Prajwal G Majjigi <prajwal1majjigi@gmail.com>
2024-02-05 22:21:39 +0530

    Functionality to order and display orders

--- models/user.js
+++ models/user.js
@@ -54,6 +54,19 @@ userSchema.methods.addToCart = function(product) {
     return this.save()
   }
 
+
+  userSchema.methods.addOrder = function(){
+          const order = this.cart.items
+          this.cart.items = []
+          return this.save()
+  }
+
+  userSchema.methods.clearCart = function() {
+    this.cart = { items: [] };
+    return this.save();
+  };
+  
+
 module.exports = mongoose.model('User', userSchema);
 
 
